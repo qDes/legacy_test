45bbfae968f72e67da03b521a90df0e2077534c4
Andrei K <1____1@bk.ru>
2020-01-21 14:15:23 +0300

    refactor Duck wrt Animal class

--- ducks.py
+++ ducks.py
@@ -1,35 +1,11 @@
-class Duck():
-    def __init__(self, name, energy=100, init_parameters=dict()):
-        if init_parameters:
-            self.name = init_parameters['name']
-            if 'energy' in init_parameters.keys():
-                condition = init_parameters["energy"]
-            else:
-                condition = energy
-        else:
-            self.name = name
-            condition = energy
-
-        self.energy = condition
+from animals import Animal
 
 
-    def say(self):
-        print("Hello, i'm Duck and my name is "+str(self.name))
+class Duck(Animal):
+    @property
+    def swim_energy(self):
+        return 10
 
-
-    def run(self):
-        print("My name is "+str(self.name)+' and i can\'t run')
-
-
-    def swim(self):
-        print("My name is "+str(self.name)+" and i swimming")
-        self.energy = self.energy - 10
-
-
-    def fly(self):
-        print("My name is "+str(self.name)+" and i flying")
-        self.energy = self.energy - 30
-
-
-    def get_energy(self):
-        return self.energy
+    @property
+    def fly_energy(self):
+        return 30
